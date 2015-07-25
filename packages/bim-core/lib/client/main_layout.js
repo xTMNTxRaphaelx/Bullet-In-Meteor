f3ece0014a62d710ce6af654582745fe28ebd729
Rahul Garg <rahul_garg08@yahoo.com>
2015-07-26 03:43:36 +0530

    Fixed Add button event for logged out user.

--- packages/bim-core/lib/client/main_layout.js
+++ packages/bim-core/lib/client/main_layout.js
@@ -3,7 +3,19 @@
 /* global Transitioner */
 Template.mainLayout.helpers({
     activeIfTemplateIs: function (template) {
-      var currentRoute = Router.current();
-      return currentRoute && template === currentRoute.lookupTemplate() ? 'active' : '';
+        var currentRoute = Router.current();
+        return currentRoute && template === currentRoute.lookupTemplate() ? 'active' : '';
     }
- });
+});
+
+Template.mainLayout.events({
+    'click .add-bulletin-btn': function (event) {
+        var user = Meteor.user();
+        if (!user) {
+            event.preventDefault();
+            event.stopPropagation();
+            sweetAlert("No Login", "You need to log in first before adding Bulletin!", "error");
+            throw new Meteor.Error(401, 'You need to log in first');
+        }
+    }
+});
--- packages/bim-world-bulletin/lib/client/template.js
+++ packages/bim-world-bulletin/lib/client/template.js
@@ -11,15 +11,6 @@ Template.worldBulletin.events({
   'click .js-loadMore button': function () {
     BiM.loadMore({ force: true });
     
-  },
-  'click .btn.btn-primary': function(event) {
-    var user = Meteor.user();
-    if (!user) {
-      event.preventDefault();
-      event.stopPropagation();
-      sweetAlert("No Login", "You need to log in first before adding Bulletin!", "error");
-      throw new Meteor.Error(401, 'You need to log in first');
-    }
   }
 });
 
